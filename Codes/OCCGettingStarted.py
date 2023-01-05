3a9adfddcdb8c74ccbd7b9442a7e87e7a4930f42
Patrick Farrell <patrick.farrell@maths.ox.ac.uk>
2023-01-05 19:07:00 +0000

    Some minor edits

--- Codes/OCCGettingStarted.py
+++ Codes/OCCGettingStarted.py
@@ -2,9 +2,9 @@ from firedrake import *
 from netgen.occ import *
 box = Box(Pnt(0,0,0), Pnt(1,1,1))
 cyl = Cylinder(Pnt(1,0.5,0.5), X, r=0.3, h=0.5)
-solid1 = box+cyl
+solid1 = box + cyl
 solid2 = solid1.Rotate(Axis((0,0,0),Y),180).Move((2.5,0.,1.))
-solid = solid2+solid1
+solid = solid2 + solid1
 geo = OCCGeometry(solid)
 ngmesh = geo.GenerateMesh(maxh=0.1)
 msh = Mesh(ngmesh)
